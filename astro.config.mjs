d526d2f0a8144d246c4c1664e132b4d089cd74b7
Dannie Vinther <davi@kea.dk>
2024-10-18 21:36:16 +0200

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: netlify(),
+  adapter: vercel(),
   image: {
     domains: ["kea-alt-del.dk"],
   },
